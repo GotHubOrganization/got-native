04783df970235dfb879b0a4b9da80382226f67ce
vincenzkoop <vincenz.koop@digitalunit.berlin>
2018-06-03 09:30:03 +0200

    finished house list with delete row

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import Expo from 'expo';
+import { Button, Icon } from 'native-base';
 import * as React from 'react';
 import { View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-import LucyChat from './src/ChatScreen/LucyChat';
 import { HomeScreen } from './src/HomeScreen/HomeScreen';
-import Profile from './src/ProfileScreen/Profile';
+import { HouseScreen } from './src/HouseScreen/HouseScreen';
 
-
-const RootStack = createStackNavigator({
-    Home: HomeScreen,
-    Profile,
-    Chat: LucyChat,
-});
+const RootStack = createStackNavigator(
+    {
+        Home: HomeScreen,
+        House: HouseScreen
+    }, {
+        initialRouteName: 'Home'
+    }
+);
 
 export default class App extends React.Component {
     public render() {
--- src/HomeScreen/HomeScreen.tsx
+++ src/HomeScreen/HomeScreen.tsx
@@ -1,45 +1,85 @@
-import { Body, Button, Card, CardItem, Container, Content, Header, Icon, Left, Right, Text, Title } from 'native-base';
+import { Body, Button, Card, CardItem, Container, Content, Header, Icon, Left, List, ListItem, Right, Text, Title } from 'native-base';
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { ListView, StatusBar } from 'react-native';
+import { NavigationScreenProp } from 'react-navigation';
 
-export class HomeScreen extends React.Component<{ navigation?: any }> {
+export class HomeScreen extends React.Component<{ navigation?: any }, { houses: any }> {
+    public static navigationOptions = ({ navigation }) => {
+        const params = navigation.state.params || {};
+        return {
+            headerStyle: {},
+            title: 'Home',
+            headerRight: (
+                <Button
+                    transparent
+                    onPress={params.newHouse}
+                >
+                    <Icon name="add" />
+                </Button>
+            )
+        };
+    };
+
+    private houses;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            houses: [
+                {
+                    name: 'Haus 1',
+                },
+            ]
+        };
+        this.houses = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.addHouse = this.addHouse.bind(this);
+        this.newHouse = this.newHouse.bind(this);
+    }
+
+    public componentWillMount() {
+        this.props.navigation.setParams({ newHouse: this.newHouse });
+    }
+
     public render() {
-        console.log(this.props);
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         return (
             <Container>
-                <Header>
-                    <Left>
-                        <Button
-                            transparent
-                            onPress={() => this.props.navigation.navigate('DrawerOpen')}>
-                            <Icon name="menu" />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>HomeScreen</Title>
-                    </Body>
-                    <Right />
-                </Header>
-                <Content padder>
-                    <Card>
-                        <CardItem>
-                            <Body>
-                                <Text>Chat App to talk some awesome people!</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                    <Button full rounded dark
-                        style={{ marginTop: 10 }}
-                        onPress={() => this.props.navigation.navigate('Chat')}>
-                        <Text>Chat With People</Text>
-                    </Button>
-                    <Button full rounded primary
-                        style={{ marginTop: 10 }}
-                        onPress={() => this.props.navigation.navigate('Profile')}>
-                        <Text>Goto Profiles</Text>
-                    </Button>
+                <Content>
+                    <List
+                        dataSource={this.houses.cloneWithRows(this.state.houses)}
+                        renderRow={house =>
+                            <ListItem>
+                                <Text> {house.name} </Text>
+                            </ListItem>}
+                        renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+                            <Button full danger onPress={_ => this.deleteHouse(secId, rowId, rowMap)}>
+                                <Icon active name="trash" />
+                            </Button>}
+                        disableRightSwipe
+                        rightOpenValue={-75}
+                    />
                 </Content>
             </Container>
         );
+    }
+
+    private deleteHouse(secId, rowId, rowMap) {
+        rowMap[`${secId}${rowId}`].props.closeRow();
+        const newData = [...this.state.houses];
+        newData.splice(rowId, 1);
+        this.setState({ houses: newData });
+    }
+
+    private newHouse() {
+        this.props.navigation.navigate('House', { addHouse: this.addHouse });
+    }
+
+    private addHouse(newHouse) {
+        this.setState({
+            houses: [
+                ...this.state.houses,
+                newHouse,
+            ]
+        });
     }
 }
